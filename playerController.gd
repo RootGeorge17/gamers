60216a682d30859958a0ad18fe3f14d762eb9cba
devero11 <casavaspun@gmail.com>
2024-12-01 19:31:58 +0200

    lol

---
+++ playerController.gd
@@ -0,0 +1,21 @@
+extends CharacterBody3D
+
+@export var SPEED: float = 5.0
+
+func _physics_process(delta: float) -> void:
+
+#Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+#Adjust velocity based on input
+	if direction != Vector3.ZERO:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	# Apply movement
+	print(velocity.x)
+	move_and_slide()
